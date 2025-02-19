c5367c0b00a4c8679927625b358eaa5217327e33
casp3rvpn <33877926+casp3rvpn@users.noreply.github.com>
2025-02-19 13:38:59 +0300

    Edit run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-export YANDEX_SUGGEST_API_KEY="8342d000-fbe3-45f8-9f99-cfec6c48db02"
-export YANDEX_GEOCODING_API_KEY="015b5b2e-7e33-4306-991d-6a36ad5a3283"
+export YANDEX_SUGGEST_API_KEY=""
+export YANDEX_GEOCODING_API_KEY=""
 nohup python3 server.py > log.txt 2>&1 &
